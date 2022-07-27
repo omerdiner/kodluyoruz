5dcf10ea74d72f512bbb391d48e125904976c693
omerdiner <99878873+omerdiner@users.noreply.github.com>
2022-07-27 14:54:36 +0300

    Update Main.java
    user input (table size) added

--- Bitirme Projesi - java 101/Main.java
+++ Bitirme Projesi - java 101/Main.java
@@ -1,8 +1,17 @@
 package MineSweeper;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        MineSweeper game = new MineSweeper(4, 4);
+        Scanner scanner = new Scanner(System.in);
+        int row, column;
+        System.out.print("Row count: ");
+        row = scanner.nextInt();
+        System.out.print("Column count: ");
+        column = scanner.nextInt();
+
+        MineSweeper game = new MineSweeper(row, column);
         game.run();
     }
 
